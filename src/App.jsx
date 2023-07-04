db2f48d19b56acfa48d6c15cbd8635698adcc81c
Adri-0311 <77983983+Adri-0311@users.noreply.github.com>
2023-07-04 18:18:56 +0200

    Extract QuoteBox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,11 @@
-import { useRef, useEffect } from 'react';
-import { fetchQuotes } from './service/fetchQuotes';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { newQuote } from './features/quote/quoteSlice';
+import QuoteBox from './features/quote/QuoteBox';
 
 function App() {
-  // Establecer el acceso al estado de la store de redux
-  const quoteState = useSelector((state) => state.quote);
-  // Declarar el actualizador de la store
-  const dispatch = useDispatch();
-
-  const buttonTweet = useRef(null);
-  const INIT_STATE = {
-    quote: 'Loading quote...',
-    author: 'Loading author...',
-  };
-  let quotesDataRef = useRef([]);
-
-  useEffect(() => {
-    fetchQuotes()
-      .catch((error) => console.error(error))
-      .then((jsonData) => {
-        // almacenar el array de citas obtenidas
-        quotesDataRef.current = jsonData.quotes;
-      })
-      .finally(() => {
-        getQuote();
-      });
-  }, []);
-
-  const setTweetLink = ({ quote, author } = INIT_STATE) => {
-    buttonTweet.current.href = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(
-      `"${quote}" ${author}`
-    )}`;
-  };
-
-  /**
-   * Obtain the new quote to generate the render and tweet link
-   */
-  const getQuote = () => {
-    const quote = selectRandomQuote();
-    // Actualizando la store de redux
-    dispatch(newQuote(quote)); 
-    setTweetLink(quote);
-  };
-
-  /**
-   * Extrac quote object from quotesDataRef array
-   * @returns {quote, author}
-   */
-  const selectRandomQuote = () => {
-    if (quotesDataRef.current.length === 0) {
-      return { quote: 'No more quotes avalible, sorry.' };
-    }
-    const random = Math.floor(Math.random() * quotesDataRef.current.length);
-    const quote = quotesDataRef.current[random];
-    // Delete from quotesData array the extracted quote to prevent duplicies
-    quotesDataRef.current.splice(random, 1);
-    return quote;
-  };
-
   return (
     <div className='container'>
       <div className='row justify-content-center align-items-center'>
-        <div className='col-8 p-4 mb-4 rounded' id='quote-box'>
-          <blockquote id='text' className='text-center'>
-            <p>
-              <i className='me-2 fa-solid fa-quote-left'></i>
-              {quoteState.quote}
-              <i className='me-2 fa-solid fa-quote-right'></i>
-            </p>
-          </blockquote>
-          <p id='author' className='text-end'>
-            <span>-</span>
-            &nbsp;
-            {quoteState.author}
-          </p>
-          <div className='btn-container d-flex justify-content-around'>
-            <button
-              className='btn btn-success'
-              id='new-quote'
-              onClick={getQuote}
-            >
-              New quote
-            </button>
-            <a
-              className='btn btn-primary'
-              id='tweet-quote'
-              href=''
-              target='_blank'
-              ref={buttonTweet}
-            >
-              Post in <i className='fa-brands fa-twitter'></i>
-            </a>
-          </div>
-        </div>
+        <QuoteBox />
         <p className='text-center'>
           Original app:&nbsp;
           <a
---
+++ src/features/quote/QuoteBox.jsx
@@ -0,0 +1,93 @@
+import { useRef, useEffect } from 'react';
+import { fetchQuotes } from '../../service/fetchQuotes';
+import { useDispatch, useSelector } from 'react-redux';
+import { newQuote } from './quoteSlice';
+import './QuoteBox.css';
+
+export default function QuoteBox() {
+  // Establecer el acceso al estado de la store de redux
+  const quoteState = useSelector((state) => state.quote);
+  // Declarar el actualizador de la store
+  const dispatch = useDispatch();
+
+  const buttonTweet = useRef(null);
+  const INIT_STATE = {
+    quote: 'Loading quote...',
+    author: 'Loading author...',
+  };
+  let quotesDataRef = useRef([]);
+
+  useEffect(() => {
+    fetchQuotes()
+      .catch((error) => console.error(error))
+      .then((jsonData) => {
+        // almacenar el array de citas obtenidas
+        quotesDataRef.current = jsonData.quotes;
+      })
+      .finally(() => {
+        getQuote();
+      });
+  }, []);
+
+  const setTweetLink = ({ quote, author } = INIT_STATE) => {
+    buttonTweet.current.href = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(
+      `"${quote}" ${author}`
+    )}`;
+  };
+
+  /**
+   * Obtain the new quote to generate the render and tweet link
+   */
+  const getQuote = () => {
+    const quote = selectRandomQuote();
+    // Actualizando la store de redux
+    dispatch(newQuote(quote));
+    setTweetLink(quote);
+  };
+
+  /**
+   * Extrac quote object from quotesDataRef array
+   * @returns {quote, author}
+   */
+  const selectRandomQuote = () => {
+    if (quotesDataRef.current.length === 0) {
+      return { quote: 'No more quotes avalible, sorry.' };
+    }
+    const random = Math.floor(Math.random() * quotesDataRef.current.length);
+    const quote = quotesDataRef.current[random];
+    // Delete from quotesData array the extracted quote to prevent duplicies
+    quotesDataRef.current.splice(random, 1);
+    return quote;
+  };
+
+  return (
+    <div className='col-8 p-4 mb-4 rounded' id='quote-box'>
+      <blockquote id='text' className='text-center'>
+        <p>
+          <i className='me-2 fa-solid fa-quote-left'></i>
+          {quoteState.quote}
+          <i className='me-2 fa-solid fa-quote-right'></i>
+        </p>
+      </blockquote>
+      <p id='author' className='text-end'>
+        <span>-</span>
+        &nbsp;
+        {quoteState.author}
+      </p>
+      <div className='btn-container d-flex justify-content-around'>
+        <button className='btn btn-success' id='new-quote' onClick={getQuote}>
+          New quote
+        </button>
+        <a
+          className='btn btn-primary'
+          id='tweet-quote'
+          href=''
+          target='_blank'
+          ref={buttonTweet}
+        >
+          Post in <i className='fa-brands fa-twitter'></i>
+        </a>
+      </div>
+    </div>
+  );
+}
